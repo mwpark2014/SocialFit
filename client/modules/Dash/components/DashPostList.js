1b31cca7b2fe6efa5ad002120452b2c04bbeba0f
Mason <park.mason.w@gmail.com>
2018-02-15 22:31:35 -0500

    Basic dashboard page (can post on own wall, not associated with user accounts yet)

--- client/modules/Dash/components/DashPostList.js
+++ client/modules/Dash/components/DashPostList.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import DashPost from '../components/DashPost';
+import PropTypes from 'prop-types';
 
 // Import style
 // import style from './style';
 
-export function DashPostList(props) {
-  // let commentNodes = this.props.data.map(comment => {
-  //     return (
-  //         <Comment author={comment.author} key={ comment._id }>
-  //             {comment.text}
-  //         </Comment>
-  //     )
-  // })
-
-  return (
-    <DashPost />
-  );
+export class DashPostList extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+  render() {
+    return (
+      <div>
+        {
+          this.props.data.map(post => (
+            <DashPost
+              post={post}
+              key={post.cuid}
+            />
+          ))
+        }
+      </div>
+    );
+  }
 }
+
+DashPostList.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  data: PropTypes.array.isRequired,
+};
+
 export default DashPostList;
--- client/modules/Dash/pages/DashboardPage.js
+++ client/modules/Dash/pages/DashboardPage.js
@@ -6,7 +6,7 @@ import DashPostList from '../components/DashPostList';
 import DashPostBox from '../components/DashPostBox';
 
 // Import Style
-// import styles from './DashboardPage.css';
+import styles from './DashboardPage.css';
 
 // Import Actions
 import { protectedTest } from '../../Auth/AuthActions';
@@ -25,26 +25,35 @@ export class DashboardPage extends Component {
 
   render() {
     return (
-      <Row>
-        <Col md={3} />
-        <Col md={9}>
-          <DashPostBox />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare lorem mi, id efficitur ipsum faucibus vel. Vivamus quis vulputate lorem. Suspendisse suscipit suscipit tortor, venenatis ornare erat egestas ac. Nam blandit consequat turpis sed dictum. Aliquam ut diam vel ante pellentesque dictum ut ullamcorper justo. Proin et tempor velit, quis vehicula nulla. In viverra tellus ac est efficitur, sed pulvinar sem bibendum. Curabitur nibh tortor, dictum eu porta vitae, facilisis nec lacus.</p>
-          <p>Vestibulum non felis pretium nulla scelerisque elementum. Praesent et mattis elit. Fusce congue dui lacus, sit amet pulvinar justo vehicula eu. Phasellus posuere tortor nunc, vel sagittis tortor interdum sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vulputate nec odio ut fermentum. Donec accumsan augue nec nunc tincidunt auctor. Nam euismod ornare orci vel iaculis. Proin suscipit aliquet diam, id varius quam aliquet at. Nullam lacinia augue sed massa sollicitudin ornare.</p>
-          <DashPostList />
-        </Col>
-      </Row>
+      <div className={styles.container}>
+        <Row>
+          <Col md={2} />
+          <Col md={10}>
+            <DashPostBox />
+            <div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare lorem mi, id efficitur ipsum faucibus vel. Vivamus quis vulputate lorem. Suspendisse suscipit suscipit tortor, venenatis ornare erat egestas ac. Nam blandit consequat turpis sed dictum. Aliquam ut diam vel ante pellentesque dictum ut ullamcorper justo. Proin et tempor velit, quis vehicula nulla. In viverra tellus ac est efficitur, sed pulvinar sem bibendum. Curabitur nibh tortor, dictum eu porta vitae, facilisis nec lacus.</p>
+              <p>Vestibulum non felis pretium nulla scelerisque elementum. Praesent et mattis elit. Fusce congue dui lacus, sit amet pulvinar justo vehicula eu. Phasellus posuere tortor nunc, vel sagittis tortor interdum sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vulputate nec odio ut fermentum. Donec accumsan augue nec nunc tincidunt auctor. Nam euismod ornare orci vel iaculis. Proin suscipit aliquet diam, id varius quam aliquet at. Nullam lacinia augue sed massa sollicitudin ornare.</p>
+            </div>
+            <DashPostList data={this.props.data} fetchPosts={this.props.fetchPosts} />
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
 
 function mapStateToProps(state) {
-  return { content: state.auth.content };
+  return {
+    content: state.auth.content,
+    data: getPosts(state),
+  };
 }
 
 DashboardPage.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
   protectedTest: PropTypes.func.isRequired,
   history: PropTypes.object.isRequired,
+  data: PropTypes.array.isRequired,
 };
 
-export default connect(mapStateToProps, { protectedTest })(DashboardPage);
+export default connect(mapStateToProps, { protectedTest, fetchPosts })(DashboardPage);
