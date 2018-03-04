b71aee15791dce3190c490af09119ecee8640036
Mason <park.mason.w@gmail.com>
2018-03-04 15:19:11 -0500

    Naive jest snapshots for regression testing for rendering components Header, Footer, Login, Register, WelcomePage

--- client/modules/App/__tests__/Components/Header.test.js
+++ client/modules/App/__tests__/Components/Header.test.js
@@ -2,23 +2,22 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import renderer from 'react-test-renderer';
 
-const activateLoginButton = () => {
-};
+const activateLoginButton = jest.fn(); //eslint-disable-line
 
-const deactivateLoginButton = () => {
-};
+const deactivateLoginButton = jest.fn(); //eslint-disable-line
 
-const redirectToWelcomePage = () => {
-};
+const redirectToWelcomePage = jest.fn(); //eslint-disable-line
 
-test('Header snapshot', () => {
-  const component = renderer.create(
-    <Header
-      authenticated={false} activateLoginButton={activateLoginButton}
-      deactivateLoginButton={deactivateLoginButton} loginButton
-      redirectToWelcomePage={redirectToWelcomePage} location={{ pathname: '/' }}
-    />,
-  );
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot(); // eslint-disable-line
+describe('Test render & styles', () => {
+  test('Header snapshot', () => {
+    const component = renderer.create(
+      <Header
+        authenticated={false} activateLoginButton={activateLoginButton}
+        deactivateLoginButton={deactivateLoginButton} loginButton
+        redirectToWelcomePage={redirectToWelcomePage} location={{ pathname: '/' }}
+      />,
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot(); // eslint-disable-line
+  });
 });
