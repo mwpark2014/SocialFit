fcaf35d289167c933e32503ef8a06830e68384db
Mason <park.mason.w@gmail.com>
2018-04-27 17:52:37 -0700

    Remove webpack imports on production (to allow pruning of devdependencies)

--- server/server.js
+++ server/server.js
@@ -5,17 +5,17 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import IntlWrapper from '../client/modules/Intl/IntlWrapper';
 
-// Webpack Requirements
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-
 // Initialize the Express App
 const app = new Express();
 
 // Run Webpack dev server in development mode
 if (process.env.NODE_ENV === 'development') {
+    // Webpack Requirements
+  const webpack = require('webpack');
+  const config =  require('../webpack.config.dev');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+
   const compiler = webpack(config);
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
   app.use(webpackHotMiddleware(compiler));
